e36440a559699c53aed1cb58d883ef27c679df34
Faith Chikwekwe <faithchikwekwe01@gmail.com>
2019-01-05 15:28:25 -0800

    📓

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 import markov
 from flask import Flask, render_template, jsonify
 
-app = Flask(__name__)
+APP = Flask(__name__)
 
 SOURCE_TEXT = 'girl.txt'
 NAME_LIST = markov.list_names(SOURCE_TEXT)
 CLEANED_TEXT = markov.cleanup(SOURCE_TEXT)
 TEXT_LIST = markov.tokenize(CLEANED_TEXT)
 
-@app.route('/')
+@APP.route('/')
 
 def index():
     """ returns a web template"""
@@ -17,7 +17,7 @@ def index():
     return render_template('index.html', index_name=index_name)
 
 
-@app.route('/api')
+@APP.route('/api')
 
 def return_json():
     """ returns json result """
