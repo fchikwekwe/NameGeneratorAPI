0332d08dcfdaffe459d64c49dea0cf0828f0b222
Faith Chikwekwe <faithchikwekwe01@gmail.com>
2019-01-07 12:33:20 -0800

    ✒️ updated README with Demonstration and Testing sections

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-This API processes submitted form data from Name-ly application through a Markov chain and returns results. 
+# Name-ly API
+
+## What is Name-ly?
+**Description Here**
+
+This API processes submitted form data from Name-ly application through a Markov chain and returns results.
+
+## Demonstration
+To see this API at work, check out https://name-ly-api.herokuapp.com/api for JSON values or https://name-ly-api.herokuapp.com/index for a web template with results.
+
+To run the app locally, after cloning this repo and running your virtual environment the app can be viewed at http://127.0.0.1:5000.
+
+## Testing
+[WIP] This API uses Pytest for testing.
+
+#### Contributor(s): Faith Chikwekwe 👩🏾‍💻
--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import markov
-from flask import Flask, render_template, jsonify
+from flask import Flask, request, render_template, jsonify
 
 app = Flask(__name__)
 
@@ -8,19 +8,27 @@ NAME_LIST = markov.list_names(SOURCE_TEXT)
 CLEANED_TEXT = markov.cleanup(SOURCE_TEXT)
 TEXT_LIST = markov.tokenize(CLEANED_TEXT)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
+def json():
+    """ takes in POST data from Node app and dumps JSON values to make them
+    accessible to rest of Flask app """
+    params = {
+        'questionOne': request.values.get('questionOne'),
+        'questionTwo': request.values.get('questionTwo'),
+        'questionThree': request.values.get('questionThree')
+    }
+    print(json.dumps(params))
+    return json.dumps(params)
 
+@app.route('/index')
 def index():
-    """ returns a web template"""
+    """ demonstrates api; returns a web template"""
     index_name = markov.main(NAME_LIST, TEXT_LIST)
-
     return render_template('index.html', index_name=index_name)
 
 
 @app.route('/api')
-
 def return_json():
-    """ returns json result """
+    """ demonstrates api; returns json result """
     json_name = markov.main(NAME_LIST, TEXT_LIST)
-
     return jsonify(json_name)
