5e11090133104000e34311f1d783e7785a0077ee
Faith Chikwekwe <faithchikwekwe01@gmail.com>
2019-01-07 19:59:37 -0800

    🔖 improvements to documentation to make code more readable; restructured main method in markov.py to make overall project more modular and usable

--- markov.py
+++ markov.py
@@ -1,5 +1,5 @@
 """ Series of functions that take in a text file, clean it, tokenize it and make
-it into a markov chain. The output is made into a sentence based on the
+it into a markov chain. The output is made into a name based on the
 conditions established below. The entire process can also be benchmarked for
 improving algorithmic speed """
 
@@ -11,13 +11,13 @@ import sample # to get words for sentence
 from dictogram import Dictogram
 
 def copy_text(input_text, output_text):
-    """ copies text from input file to corpus file """
+    """ Copies text from input file to corpus file. """
     with open(input_text, 'r') as in_text, open(output_text, 'a') as out_text:
         out_text.write(in_text.read())
 
 def make_source_text(input_one, input_two, input_three, output_text):
     """ Clear out the output file, copy text from the input files and return
-    the file with new contents"""
+    the file with new contents. """
     # clear out the previous file contents
     open(output_text, 'w').close()
     # copy from three input files based on question answers
@@ -27,8 +27,8 @@ def make_source_text(input_one, input_two, input_three, output_text):
     return output_text
 
 def cleanup(text):
-    """ takes in a text file, opens it and cleans text using regex. outputs
-    string of cleaned text """
+    """ Takes in a text file, opens it and cleans text using regex and outputs
+    string of cleaned text. """
     with open(text, 'r') as uncleaned_text:
         no_chapters = re.sub('[A-Z]{3,}', ' ', uncleaned_text.read())
         remove_periods = re.sub('(\s\.){4,}', '', no_chapters)
@@ -36,14 +36,14 @@ def cleanup(text):
     return new_text
 
 def tokenize(text):
-    """ takes in cleaned text as string and makes it into a list of tokens """
+    """ Takes in cleaned text as string and makes it into a list of tokens. """
     source = list(text.rstrip().replace('\n', ' '))
     return source
 
 # takes in list of words
 def first_order_markov(text_list):
-    """ takes in a word and checks to see what words come after it
-    to determine the word sequence for our generated markov chain"""
+    """ Takes in a letter and checks to see what letters come after it
+    to determine the letters sequence for our generated markov chain. """
     markov_dict = dict()
     # for each word in list, key is word and value is dictogram
     for index in range(len(text_list) - 1):
@@ -60,8 +60,8 @@ def first_order_markov(text_list):
     return markov_dict
 
 def nth_order_markov(order, text_list):
-    """ this function takes in a word and checks to see what words come after it
-    to determine the word sequence for our generated markov chain"""
+    """ Takes in a letter and checks to see what letters come after it to
+    determine the letter sequence for our generated markov chain. """
     markov_dict = dict()
     # for each word in list, key is word and value is dictogram
     for index in range(len(text_list) - order):
@@ -79,7 +79,7 @@ def nth_order_markov(order, text_list):
 
 def start_tokens(dictionary):
     """ Get words that can start a sentence; this method is O(n) worst case
-    because one must check every word in the corpus"""
+    because one must check every letter in the corpus. """
     start_token_list = []
     for key in dictionary:
         if key[0].islower() is False and key[0].endswith('.') is False:
@@ -88,7 +88,7 @@ def start_tokens(dictionary):
     return token
 
 def stop_token(dictionary):
-    """ Get words that can end a sentence"""
+    """ Get letters that have been seen ending a name before. """
     stop_tokens = []
     for key, value in dictionary.items():
         # the key number must be changed depending on order number
@@ -97,15 +97,16 @@ def stop_token(dictionary):
     return stop_tokens
 
 def list_names(text):
-    """ creates list of names that were in original list to check against
-        so that the new name is not a duplicate of names in the list"""
+    """ Creates list of names that were in original list to check against
+        so that the new name is not a duplicate of names that were in the
+        corpus. """
     with open(text, 'r') as names:
         list_of_names = list(names)
         name_list = [name.strip() for name in list_of_names]
     return name_list
 
 def create_name(start_token, dictionary):
-    """ takes dictionary, start and end tokens and makes a sentence """
+    """ Takes dictionary, start and end tokens and makes a sentence. """
     # create sentence and add first word
     name = []
     # this is hard coded; must be changed to fit the order number; currently second
@@ -115,7 +116,7 @@ def create_name(start_token, dictionary):
 
     current_token = start_token
     # stop when current_token is a stop token
-    while not current_token[1].isspace() and len(name) <= 15:
+    while not current_token[1].isspace() and len(name) <= 10:
         for key, value in dictionary.items():
             if key == current_token:
                 # sample from histogram of values
@@ -125,7 +126,7 @@ def create_name(start_token, dictionary):
                 name.append(sample_letter)
                 # assign second word of key and value to current token
                 # this is hard coded; must be changed to fit the order number
-                # I am unpacking the current token
+                # unpacking the current token
                 (current_token_one, current_token_two) = current_token
                 current_token = (current_token_two, sample_letter)
                 # get out of for loop and start process over
@@ -133,7 +134,7 @@ def create_name(start_token, dictionary):
     return name
 
 def logger(start_time, file):
-    """ Benchmarking to improve performance """
+    """ Benchmarking to improve performance. """
     with open(file, "a") as text:
         text.write("""
 
@@ -144,11 +145,11 @@ def logger(start_time, file):
     return 'hello'
 
 def main(input_one, input_two, input_three, output):
-    """ takes in three text file names as first three parameters
+    """ Takes in three text file names as first three parameters
         takes in output (corpus) file name as the final value
-        defines variables and calls functions sequentially
+        defines variables and calls functions sequentially.
 
-        returns generated name(s) """
+        Returns generated name(s). """
 
     # Make and clean up corpus
     corpus = make_source_text(input_one, input_two, input_three, output)
@@ -164,8 +165,9 @@ def main(input_one, input_two, input_three, output):
         # Make word and remove whitespace
         first_name = "".join(markov_list).strip()
         # Check if name is match for any name in list; if so, start over
+        # Name list contains the source names; ensures generated names are unique
         if first_name in name_list:
-            print("not valid name", first_name)
+            print("duplicate name", first_name)
             continue
 
         # Return the valid name
@@ -176,7 +178,7 @@ def main(input_one, input_two, input_three, output):
 if __name__ == '__main__':
     # start_time = time.process_time()
 
-    # pass in text names as first three values and output corpus as final value
+    # pass in text files as first three values and output corpus file as fourth value
     main('girl.txt', 'app_names.txt', 'modern.txt', 'corpus.txt')
 
     # logger(start_time, 'markov_logger.txt')
